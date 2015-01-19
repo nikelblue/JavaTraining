d44547f0bace5277fd6f18991bff38af8221bf4d
nikelblue <nikelblue@users.noreply.github.com>
2015-01-19 15:05:31 +0200

    Initialize Eclipse Java project

---
+++ src/org/training/warmup/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.training.warmup;
+
+/**
+ * @author nikelblue
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		printString("Hello Victoria!!!");
+	}
+	
+	private static void printString(String str) {
+		System.out.println(str);
+	}
+
+}
