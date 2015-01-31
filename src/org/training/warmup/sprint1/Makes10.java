ededdb0f1dd78eb3cc1c2162b1ce04c338936291
VictoriaJol <jjsfsu@hotmail.com>
2015-01-31 18:23:46 +0800

    Story 8095: makes10

---
+++ src/org/training/warmup/sprint1/Makes10.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.training.warmup.sprint1;
+
+/**
+ * @author jingjing
+ *
+ */
+public class Makes10 {
+
+	
+	/**
+	 * Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return 
+	 *      true if one if them is 10 or if their sum is 10
+	 */
+	
+	public static boolean makes10(int a, int b) {
+		return a == 10 || b == 10 || (a+b == 10);
+	}
+	
+
+}
