1a3581daa6f65574c16b1672b503360de0ea021b
nikelblue <nikelblue@users.noreply.github.com>
2015-02-06 23:04:03 +0200

    Story 9259: Maxspan. Being part of warmup sprint, initial approach of maxSpan is just perfect, showing a good understaning of loops and conditions in Java but not intended to focus on other aspects such as performance or readability. As part of training, I would like to offer a second approach which: - is based on the initial approach but changing the iteration order of   the inner loop allowing to detect and skip unnecesary steps. This is   increasing the performance by more than 50% for worst conditions.
    - uses different variables naming as an example of increasing the
      readability.

--- src/main/java/org/training/warmup/sprint3/Maxspan.java
+++ src/main/java/org/training/warmup/sprint3/Maxspan.java
@@ -3,18 +3,20 @@
  */
 package org.training.warmup.sprint3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author monkey
  *
  */
 public class Maxspan {
-
+	private static Logger LOGGER = LoggerFactory.getLogger(Maxspan.class);
 	
 	/**
 	 * Consider the leftmost and righmost appearances of some value in an array. 
 	 * We'll say that the "span" is the number of elements between the two inclusive. 
 	 * A single value has a span of 1. Returns the largest span found in the given array. (Efficiency is not a priority.) 
-
 	 * 
 	 * @param nums
 	 * @return
@@ -24,6 +26,8 @@ public class Maxspan {
 		int number = 0;
 		int index = 0;
 		
+		int iterations = 0; //performance marker
+		
 		if(nums.length == 1) {
 			span = 1;
 		}else {					
@@ -31,6 +35,8 @@ public class Maxspan {
 			int s = nums[i];			
 			
 			for(int j=i+1; j<nums.length; j++) {
+				iterations++; //performance marker
+				
 				int l = nums[j];			
 				
 				if(l==s) {
@@ -45,9 +51,36 @@ public class Maxspan {
 			}
 		  }
 		}
-		
+		LOGGER.debug("Number of iterations for maxSpan: " + iterations); //performance marker
 		return span;
 	}
 
+	public static int maxSpan2(int[] nums) {
+		int maxSpan = 0;
+		
+		int iterations = 0; //performance marker
+
+		for (int leftIndex = 0; leftIndex < nums.length; leftIndex++) {
+			int leftNum = nums[leftIndex];
+			
+			for (int rightIndex = nums.length - 1; rightIndex >= leftIndex ; rightIndex--) {
+				iterations++; //performance marker
+				
+				int currentSpan = rightIndex - leftIndex + 1;
+				
+				//do not continue since currentSpan is decreasing
+				//which means further values cannot be greater that maxSpan.
+				if(currentSpan < maxSpan)
+					break;
+
+				int rightNum = nums[rightIndex];
+				if(leftNum == rightNum) 
+					maxSpan = currentSpan;
+			}
+		}
+		
+		LOGGER.debug("Number of iterations for maxSpan2: " + iterations); //performance marker
+		return maxSpan;
+	}
 
 }
--- src/test/java/org/training/warmup/sprint3/MaxspanTest.java
+++ src/test/java/org/training/warmup/sprint3/MaxspanTest.java
@@ -3,27 +3,47 @@
  */
 package org.training.warmup.sprint3;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author monkey
  *
  */
 public class MaxspanTest {
+	private static Logger LOGGER = LoggerFactory.getLogger(MaxspanTest.class);
+
+	private final int[] array1 = {1, 2, 1, 1, 3};
+	private final int[] array2 = {1, 1, 2, 1, 4, 1, 4};
+	private final int[] array3 = {1};
+	private final int[] array4 = {1, 4, 2, 1, 4, 1, 3, 4};
+	private final int[] array5 = {1, 1, 2, 3, 4, 5, 6, 7};
+	private final int[] array6 = {};
+
 
 	@Test
-	public void test() {
-		int[] array1 = {1, 2, 1, 1, 3};
-		int[] array2 = {1, 1, 2, 1, 4, 1, 4};
-		int[] array3 = {1};
-		int[] array4 = {1, 4, 2, 1, 4, 1, 3, 4};
-		
+	public void testMaxSpan() {
+		LOGGER.info("Performing test for maxSpan...");
 		assertEquals("The maxpan is", 4, Maxspan.maxSpan(array1) );
 		assertEquals("The maxpan is", 6, Maxspan.maxSpan(array2) );
 		assertEquals("The maxpan is", 1, Maxspan.maxSpan(array3) );
 		assertEquals("The maxpan is", 7, Maxspan.maxSpan(array4) );
+		assertEquals("The maxpan is", 2, Maxspan.maxSpan(array5) );
+		assertEquals("The maxpan is", 0, Maxspan.maxSpan(array6) );
+	}
+
+	@Test
+	public void testMaxSpan2() {
+		LOGGER.info("Performing test for maxSpan2...");
+		assertEquals("The maxpan is", 4, Maxspan.maxSpan2(array1) );
+		assertEquals("The maxpan is", 6, Maxspan.maxSpan2(array2) );
+		assertEquals("The maxpan is", 1, Maxspan.maxSpan2(array3) );
+		assertEquals("The maxpan is", 7, Maxspan.maxSpan2(array4) );
+		assertEquals("The maxpan is", 2, Maxspan.maxSpan2(array5) );
+		assertEquals("The maxpan is", 0, Maxspan.maxSpan2(array6) );
 	}
 
 }
