036a1267e9f185a6859f40fce339a1bf4629c06c
nikelblue <nikelblue@users.noreply.github.com>
2015-02-21 21:37:28 +0200

    Inheritance: Exercise 05

---
+++ src/main/java/org/training/inheritance/exercise05/Pet.java
@@ -0,0 +1,20 @@
+package org.training.inheritance.exercise05;
+
+public class Pet {
+//	You have been given a design document for a veterinary registration system for implementation
+//	in Java. It states:
+//	
+//	¡°A pet has an owner, a registration date, and a vaccination-due date. A cat is a pet that has a
+//	flag indicating if it has been neutered, and a textual description of its markings.¡±
+//	
+//	1) Define class Pet (this class) according to the statement above.
+//	2) Given that you expect the Cat class to be used freely
+//	throughout the application, how would you make the opening declaration of the Cat class, up
+//	to but not including the first opening brace? Use only these words and spaces: boolean, Cat,
+//	class, Date, extends, Object, Owner, Pet, private, protected, public, String.
+//		A. protected class Cat extends Owner
+//		B. public class Cat extends Object
+//		C. public class Cat extends Pet
+//		D. private class Cat extends Pet
+//	Define class Cat in same package but separate file.
+}
