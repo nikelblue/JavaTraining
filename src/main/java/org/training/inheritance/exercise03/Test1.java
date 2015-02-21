4326cfebbe833e6a41c60c568f94ddb319f8c171
nikelblue <nikelblue@users.noreply.github.com>
2015-02-21 18:52:46 +0200

    Inheritance: Exercise 03

---
+++ src/main/java/org/training/inheritance/exercise03/Test1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.training.inheritance.exercise03;
+
+/**
+ * !!! TRY TO ANSWEER BEFORE WRITING ANY PIECE OF CODE
+ * 
+ * Which of the following methods would be legal if added (individually) at line 23? (Choose all
+ * that apply.)
+ * A. public int aMethod(int a, int b) { }
+ * B. public float aMethod(float a, float b) { }
+ * C. public float aMethod(float a, float b, int c) throws Exception { }
+ * D. public float aMethod(float c, float d) { }
+ * E. private float aMethod(int a, int b, int c) { }
+ * 
+ * @author daniel
+ *
+ */
+public class Test1 {
+	public float aMethod(float a, float b) {
+		return a+b;
+	}
+	
+	//new method here
+}
