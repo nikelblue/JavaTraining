5e9c8520b0ddbea3bd5dbb961c242432c2bb3949
nikelblue <nikelblue@users.noreply.github.com>
2015-02-21 19:06:48 +0200

    Inheritance: Exercise 04

---
+++ src/main/java/org/training/inheritance/exercise04/Test1.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.training.inheritance.exercise04;
+
+import java.io.IOException;
+
+/**
+ * @author daniel
+ *
+ */
+public class Test1 {
+	public float aMethod(float a, float b) throws IOException {
+		return a+b;
+	}
+}
---
+++ src/main/java/org/training/inheritance/exercise04/Test2.java
@@ -0,0 +1,13 @@
+package org.training.inheritance.exercise04;
+
+public class Test2 extends Test1 {
+// !!! TRY TO ANSWEER BEFORE WRITING ANY PIECE OF CODE
+//	Which of the following methods would be legal (individually) at line 11 in class Test2? 
+//	(Choose all that apply.)
+//			A. float aMethod(float a, float b) {...}
+//			B. public int aMethod(int a, int b) throws Exception {...}
+//			C. public float aMethod(float a, float b) throws Exception {...}
+//			D. public float aMethod(float p, float q) {...}
+	
+	
+}
