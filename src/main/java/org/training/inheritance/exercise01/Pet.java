1c68465e8287c42819e13deee865e12527f4d488
nikelblue <nikelblue@users.noreply.github.com>
2015-02-21 18:18:53 +0200

    Inheritance: Exercise 01

---
+++ src/main/java/org/training/inheritance/exercise01/Pet.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.training.inheritance.exercise01;
+
+/**
+ * <p>You have been given a design document for a veterinary registration system for implementation
+ * in Java. It states:</p>
+ * 
+ * <p>¡°A pet has an owner, a registration date, and a vaccination-due date. A cat is a pet that has a
+ * flag indicating whether it has been neutered, and a textual description of its markings.¡±</p>
+ * 
+ * <ol>
+ * <li> Define class Pet (this class) according to the statement above.</li>
+ * <li> which of the following fields would be appropriate for inclusion in the 
+ * Cat class as members? (Choose all that apply.)
+ * 		<ol type="A">
+ * 		<li> Pet thePet;
+ * 		<li> Date registered;
+ * 		<li> Date vaccinationDue;
+ * 		<li> Cat theCat;
+ * 		<li> boolean neutered;
+ * 		<li> String markings;
+ * 		</ol>
+ * Define class Cat accordingly, in same package but separate file.
+ * </ol>
+ * @author nikelblue
+ *
+ */
+public class Pet {
+
+}
