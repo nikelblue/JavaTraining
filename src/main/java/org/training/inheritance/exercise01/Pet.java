7d469f4f78b12628afca63ea2a63e6e4aec7abe8
VictoriaJol <jjsfsu@hotmail.com>
2015-02-23 22:53:27 +0800

    Inheritance: exercise01

--- src/main/java/org/training/inheritance/exercise01/Pet.java
+++ src/main/java/org/training/inheritance/exercise01/Pet.java
@@ -3,12 +3,14 @@
  */
 package org.training.inheritance.exercise01;
 
+import java.util.Date;
+
 /**
  * <p>You have been given a design document for a veterinary registration system for implementation
  * in Java. It states:</p>
  * 
- * <p>¡°A pet has an owner, a registration date, and a vaccination-due date. A cat is a pet that has a
- * flag indicating whether it has been neutered, and a textual description of its markings.¡±</p>
+ * <p>ï¿½ï¿½A pet has an owner, a registration date, and a vaccination-due date. A cat is a pet that has a
+ * flag indicating whether it has been neutered, and a textual description of its markings.ï¿½ï¿½</p>
  * 
  * <ol>
  * <li> Define class Pet (this class) according to the statement above.</li>
@@ -28,5 +30,43 @@ package org.training.inheritance.exercise01;
  *
  */
 public class Pet {
+
+	private Date registered;
+	private Date vaccinationDue;
+	private String ownerName;
+
+	public Pet() {
+		super();
+	}
+
+	public Pet(Date vaccinationDue, String ownerName, Date registered) {
+		registered = this.registered;
+		vaccinationDue = this.vaccinationDue;
+		ownerName = this.ownerName;
+	}
+
+	public Date getRegistered() {
+		return registered;
+	}
+
+	public void setRegistered(Date registered) {
+		this.registered = registered;
+	}
+
+	public Date getVaccinationDue() {
+		return vaccinationDue;
+	}
+
+	public void setVaccinationDue(Date vaccinationDue) {
+		this.vaccinationDue = vaccinationDue;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
 
 }
