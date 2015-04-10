509fa53fb6116009495155b2ba5839ae33a38099
VictoriaJol <jjsfsu@hotmail.com>
2015-04-10 23:08:15 +0800

    Stories 1820:sumFloats, 18225:compareInt

--- src/test/java/org/training/warmup/sprint7/NumbersTest.java
+++ src/test/java/org/training/warmup/sprint7/NumbersTest.java
@@ -15,9 +15,9 @@ public class NumbersTest {
 
 	@Test
 	public void sumIntegersTestSuccess() {
-		assertEquals("Expected sum integer is", 1, Numbers.sumIntegers("2", "-4", "3"));
-		assertEquals("Expected sum integer is", 2, Numbers.sumIntegers("2"));
-		assertEquals("Expected sum integer is", 0, Numbers.sumIntegers());
+		assertEquals("Unexpected sum integer :", 1, Numbers.sumIntegers("2", "-4", "3"));
+		assertEquals("Unexpected sum integer :", 2, Numbers.sumIntegers("2"));
+		assertEquals("Unexpected sum integer :", 0, Numbers.sumIntegers());
 	}
 	
 	@Test(expected = NumberFormatException.class)
@@ -33,6 +33,26 @@ public class NumbersTest {
 	@Test(expected = NumberFormatException.class)
 	public void sumIntegersTestFail3() {
 		Numbers.sumIntegers("keeee");
+	}
+	
+	
+	@Test
+	public void sumFloatsTestSuccess() {
+		assertEquals("Unexpected sum float :", 0f, Numbers.sumFloats(),0.01);
+		assertEquals("Unexpected sum float :", 2.3f, Numbers.sumFloats("2.3"),0.01);
+		assertEquals("Unexpected sum float :", 13.3f, Numbers.sumFloats("2.3", "1.1e+1", "0"),0.01);
+	}
+	
+	@Test(expected = NumberFormatException.class)
+	public void sumFloatsTestFail1() {
+		Numbers.sumFloats("o,2");	
+	}
+	
+	@Test
+	public void compareIntTest() {
+		assertEquals("Unexpected comparison result : ", "1 is less than 2", Numbers.compareInt(1,2));
+		assertEquals("Unexpected comparison result : ", "1 is equal to 1", Numbers.compareInt(1,1));
+		assertEquals("Unexpected comparison result : ", "1 is greater than 0", Numbers.compareInt(1,0));
 	}
 
 }
