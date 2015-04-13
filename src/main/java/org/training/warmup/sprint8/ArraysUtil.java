c62e39f9fe8c96cc213f4526b5ecf772f14e4515
VictoriaJol <jjsfsu@hotmail.com>
2015-04-13 23:02:41 +0800

    Story 18639: ArrayUtil

---
+++ src/main/java/org/training/warmup/sprint8/ArraysUtil.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.training.warmup.sprint8;
+
+import java.util.Arrays;
+
+/**
+ * @author monkey
+ *
+ */
+public class ArraysUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names = {"Angelina Jolie", "Leonardo DiCaprio", "Brad Pitt", "Silvester Stalone"};
+		sortNames(names);
+	}
+	
+	
+	public static void sortNames(String[] names) {
+		
+		System.out.print("Names: ");
+		System.out.println(Arrays.toString(names));
+		
+		String[] sortedNames = Arrays.copyOf(names, names.length);
+		
+		System.out.print("Sorted names:");
+
+		Arrays.sort(sortedNames);
+		System.out.println(Arrays.toString(sortedNames));
+		
+		System.out.println("The initial names array was sorted:" + Arrays.equals(sortedNames, names));
+		
+		System.out.println("The new index of \"Brad Pitt\":" + Arrays.binarySearch(sortedNames, "Brad Pitt"));	
+
+	}
+
+}
