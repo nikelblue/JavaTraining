4bd96f7563c83dc35dcb449b01e67539eaf096bc
nikelblue <nikelblue@users.noreply.github.com>
2015-05-11 16:16:41 +0300

    Threads tutorial

---
+++ src/main/java/org/training/threads/example01/Marathon.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.training.threads.example01;
+
+/**
+ * @author nikelblue
+ *
+ */
+public class Marathon {
+	 public static void main(String[] args) {
+		//We instantiate two participants to the marathon
+		Thread monkey = new MarathonRunner("Monkey");
+		Thread cupcake = new MarathonRunner("Cupcake");
+		
+		monkey.start(); //Monkey starts to run
+		cupcake.start(); //Cupcake starts to run at the same time.
+		
+		//Lets watch the output to see who is the winer
+	}
+}
