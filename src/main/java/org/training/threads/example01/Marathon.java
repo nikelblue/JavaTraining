063773516def46d0eaa331ee7c504554f9f37544
VictoriaJol <jjsfsu@hotmail.com>
2015-05-12 22:26:05 +0800

    Marathon example01

--- src/main/java/org/training/threads/example01/Marathon.java
+++ src/main/java/org/training/threads/example01/Marathon.java
@@ -8,14 +8,15 @@ package org.training.threads.example01;
  *
  */
 class Marathon {
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 		//We instantiate two participants to the marathon
 		Thread monkey = new MarathonRunner("Monkey");
 		Thread cupcake = new MarathonRunner("Cupcake");
-		
+
 		monkey.start(); //Monkey starts to run
 		cupcake.start(); //Cupcake starts to run at the same time.
-		
+		System.out.println("Main thread keeps running too");
+
 		//Lets watch the output to see who is the winer
 	}
 }
