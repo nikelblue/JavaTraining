9ccedd19398bf773c83f57c9368944973e78f0cb
nikelblue <nikelblue@users.noreply.github.com>
2015-05-19 17:33:54 +0200

    Threads tutorial: example 5.2

---
+++ src/main/java/org/training/threads/example05/OperationB.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.training.threads.example05;
+
+/**
+ * @author nikelblue
+ *
+ */
+final class OperationB extends Thread {
+	private Account account;
+
+	public OperationB(String name, Account account) {
+		super(name);
+		this.account = account;
+	}
+
+	public void run() {
+		while (true) {
+			int amount = (int) (Math.random() * 10000);
+			String name = getName();
+
+			System.out.print(name);
+			account.deposit(amount);
+			account.withdraw(amount);
+			int balance = account.getBalance();
+			System.out.println(" " + name);
+			
+			if (balance != 0) {
+				System.err.println(name + ":**balance=" + balance);
+				System.exit(1);
+			}
+		}
+	}
+
+	/**
+	 * TODO: Analyze the output. At each iteration a null operation is performed (line 24).
+	 * We expect the balance to remain 0. 
+	 * The application exits if balance is different from 0.
+	 * 
+	 * After few loops the balance will be different from 0, and application will exit.
+	 * Can you explain why? 
+	 * 
+	 * Add a fix to this problem. After fixing the code, the app should loop infinitely.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Account compte = new Account();
+		for (int i = 0; i < 20; i++) {
+			OperationB operation = new OperationB("A" + i, compte);
+			operation.start();
+		}
+	}
+
+}
